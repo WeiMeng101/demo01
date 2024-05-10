e4c0f0aa09718d3c01b1360ce9e49f84d5efb115
wei <2751338876@qq.com>
2024-05-10 12:16:03 +0800

    Initial Commit

---
+++ demo01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo01
+//
+//  Created by 魏梦 on 2024/5/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo01/demo01App.swift
@@ -0,0 +1,17 @@
+//
+//  demo01App.swift
+//  demo01
+//
+//  Created by 魏梦 on 2024/5/10.
+//
+
+import SwiftUI
+
+@main
+struct demo01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
